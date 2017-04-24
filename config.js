951119273b42779ae1b3615fb597ec4c43132dd4
Jack Grab <exexeg@gmail.com>
2017-04-24 17:56:19 +0300

    First commit

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '1178108708933917',
+        clientSecret: '8f40aa67188caab86e93f8b545e1637b',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
+}
